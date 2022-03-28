ab7d853424a2cda72b05cfd6ef68328fe444458a
RomanVinichenko <chicagezzz@gmail.com>
2022-03-29 01:43:59 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-$(function () {});
+// $(function () {});
+
+const burger = document.querySelector('.header__burger')
+const menu = document.querySelector('.menu__items')
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('menu__items--active')
+})
